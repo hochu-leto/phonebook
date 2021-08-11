c44774ea9a9b432b27f06f438d8202f6ae87b0f4
Тимофей <porno_mail@bk.ru>
2021-08-11 23:15:02 +0400

    наконец-то!!!!!!!!!!!!!!!!

--- main.py
+++ main.py
@@ -4,9 +4,10 @@
 # Предполагается, что телефон и e-mail у человека может быть только один.
 # Необходимо:
 #
-# поместить Фамилию, Имя и Отчество человека в поля lastname, firstname и surname соответственно. В записной книжке изначально может быть Ф + ИО, ФИО, а может быть сразу правильно: Ф+И+О;
-# привести все телефоны в формат +7(999)999-99-99. Если есть добавочный номер, формат будет такой: +7(999)999-99-99 доб.9999;
-# объединить все дублирующиеся записи о человеке в одну.
+# поместить Фамилию, Имя и Отчество человека в поля lastname, firstname и surname соответственно. В записной книжке
+# изначально может быть Ф + ИО, ФИО, а может быть сразу правильно: Ф+И+О; привести все телефоны в формат +7(
+# 999)999-99-99. Если есть добавочный номер, формат будет такой: +7(999)999-99-99 доб.9999; объединить все
+# дублирующиеся записи о человеке в одну.
 import re
 from pprint import pprint
 # читаем адресную книгу в формате CSV в список contacts_list
@@ -15,35 +16,22 @@ import csv
 with open("phonebook_raw.csv", encoding='utf-8') as f:
     rows = csv.reader(f, delimiter=",")
     contacts_list = list(rows)
-
+pprint(contacts_list)
 # TODO 1: выполните пункты 1-3 ДЗ
 # ваш код
 phone_pattern = re.compile(r"([\+7|8)]+)\s*\(?(\d{3})\)?[\s|-]*(\d{3})[\s|-]*(\d{2})[\s|-]*(\d{2})\s*(\(?(\w+\.)\s*("
                            r"\d+)\)?)*")
-pprint(contacts_list)
 result = []
 for contacts in contacts_list:
-    result_list = [6]
-    fio = ''
-    for i in range(4):
-        fio += contacts[i].replace(' ', ',') + ','
-    # fio.replace(' ', ',')
+    fio = ','.join(contacts[:3])
+    fio = fio.replace(' ', ',')
     result_list = fio.split(',')
-    result_list[4] = contacts[4]
-    # result_list.append('4' + contacts[4])
-    result_item = phone_pattern.sub(r"+7(\2)\3-\4-\5 \7\8 ", contacts[5])
-    result_list.append(result_item)
-    result_list.append(contacts[6])
-    # print(result_list)
-    for c in result:
-        # print(f"result[{c}] = {(c[0] + c[1])}")
-        if (c[0] + c[1]) == (result_list[0] + result_list[1]):
-            print((c[0] + c[1]))
-            for item in range(len(c) - 1):
-                print(
-                    f'В списке result_list элемент {item} - {result_list[item]} ; в списке result этот элемент -  {c[item]}')
+    result_list = result_list[:3] + contacts[3:]
+    result_list[5] = phone_pattern.sub(r"+7(\2)\3-\4-\5 \7\8 ", result_list[5]).strip()
 
-                # print(item)
+    for c in result:
+        if (c[0] + c[1]) == (result_list[0] + result_list[1]):
+            for item in range(len(c) - 1):
                 if result_list[item] == '':
                     result_list[item] = c[item]
             result.remove(c)
